11817da2cf5f957d82155cb955b9c208881606e6
Andy Le <iceinmydna@gmail.com>
2019-02-10 12:13:46 -0800

    playing around with js, trying to add it to static site

--- WEB-a.js
+++ WEB-a.js
@@ -1,16 +1,9 @@
-if (matchMedia) {
-    const mq = window.matchMedia("(min-width: 600px)");
-    mq.addListener(WidthChange);
-    WidthChange(mq);
-  }
+var navButton = document.querySelectorAll("a");
 
-function WidthChange(mq) {
-    if (mq.matches) {
-      navBar.style.display = 'flex';// window width is at least 600px
-      navBar.classList.remove('fade-in-left');
-    } else {
-      navBar.style.display = 'none';// window width is less than 600px
-    }
-  }
+for(var i = 0; i <= navButton.length; i++){
+  navButton[i].addEventListener("click", function(){
+    console.log(navButton[i]);
+  })
+}
 
- 
+
