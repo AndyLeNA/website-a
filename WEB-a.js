d1abb5c74b0ac606bc3d08bc7430447b4ce7fe64
Andy Le <iceinmydna@gmail.com>
2019-02-10 12:40:59 -0800

    console log 'clicked!' whenever an anchor tag is clicked

--- WEB-a.js
+++ WEB-a.js
@@ -2,7 +2,7 @@ var navButton = document.querySelectorAll("a");
 
 for(var i = 0; i <= navButton.length; i++){
   navButton[i].addEventListener("click", function(){
-    console.log(navButton[i]);
+    console.log("clicked!");
   })
 }
 
