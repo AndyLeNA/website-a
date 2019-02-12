e3475e0c6fabc1cb573bc95f73fdb79db3ee5beb
Andy Le <iceinmydna@gmail.com>
2019-02-12 00:08:39 -0800

    site now works on multiple devices

--- WEB-a.js
+++ WEB-a.js
@@ -1,9 +1,25 @@
-var navButton = document.querySelectorAll("a");
+var navButton = document.querySelectorAll("li");
+const navBar = document.querySelector(".main-header-right ul");
 
-for(var i = 0; i <= navButton.length; i++){
+
+for(var i = 0; i <= navButton.length - 1; i++){
   navButton[i].addEventListener("click", function(){
-    console.log("clicked!");
+    this.textContent = "idiot";
   })
 }
 
+if (matchMedia) {
+  const mq = window.matchMedia("(min-width: 600px)");
+  mq.addListener(WidthChange);
+  WidthChange(mq);
+}
 
+// media query change
+function WidthChange(mq) {
+  if (mq.matches) {
+    navBar.style.display = 'flex';// window width is at least 600px
+    navBar.classList.remove('fade-in-left');
+  } else {
+    navBar.style.display = 'none';// window width is less than 600px
+  }
+}
